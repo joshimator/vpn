2b2f380ac4bae374fe122ec5465eb29b86649716
Joshimator <joshua.neef@gmail.com>
2021-11-24 22:48:20 +0100

    changes

--- vpnserver.sh
+++ vpnserver.sh
@@ -3,8 +3,8 @@ modprobe tun
 make-cadir /root/my_ca
 cd /root/my_ca
 bash easyrsa clean-all
-bash easyrsa build-­ca nopass
-bash easyrsa gen-­dh
+bash easyrsa build-ca nopass
+bash easyrsa gen-dh
 
 bash easyrsa build-server-full server nopass
 bash easyrsa build-client-full client01 nopass
