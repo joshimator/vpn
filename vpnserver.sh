79a2cf90f767e5f96ad17c5dd7976e2cb72dc079
Joshimator <joshua.neef@gmail.com>
2021-11-24 22:44:47 +0100

    changes

--- vpnserver.sh
+++ vpnserver.sh
@@ -2,12 +2,12 @@ apt install openvpn easy-rsa -y
 modprobe tun
 make-cadir /root/my_ca
 cd /root/my_ca
-bash easyrsa clean-­all
+bash easyrsa clean-all
 bash easyrsa build-­ca nopass
 bash easyrsa gen-­dh
 
-bash easyrsa build­-server-­full server nopass
-bash easyrsa build­-client-­full client01 nopass
+bash easyrsa build-server-full server nopass
+bash easyrsa build­-client-full client01 nopass
 
 cp /root/my_ca/pki/private/server.key /etc/openvpn/
 cp /root/my_ca/pki/issued/server.crt /etc/openvpn/
