e63b118e4e1567f93ee8414f31b315f5c06ac096
joshimator <92186478+joshimator@users.noreply.github.com>
2021-11-26 09:45:41 +0100

    Create copycerts.sh

---
+++ copycerts.sh
@@ -0,0 +1,7 @@
+cp /root/my_ca/pki/private/server.key /etc/openvpn/
+cp /root/my_ca/pki/issued/server.crt /etc/openvpn/
+cp /root/my_ca/pki/ca.crt /etc/openvpn/
+cp /root/my_ca/pki/dh.pem /etc/openvpn/
+
+cp /root/my_ca/pki/issued/client01.crt /etc/openvpn/
+cp /root/my_ca/pki/private/client01.key /etc/openvpn/
