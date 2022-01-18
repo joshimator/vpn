4102a8a706f2b576653b8717f432e65b7f50304e
joshimator <92186478+joshimator@users.noreply.github.com>
2022-01-18 16:49:18 +0100

    Update vpnserver.sh

--- vpnserver.sh
+++ vpnserver.sh
@@ -27,6 +27,7 @@ echo "ca ca.crt" >> $path
 echo "cert server.crt" >> $path
 echo "key server.key" >> $path
 echo "dh dh.pem" >> $path
+echo "push route 192.168.0.0 255.255.255.0" >> $path
 echo "ping-timer-rem" >> $path
 echo "keepalive 20 180" >> $path
 
